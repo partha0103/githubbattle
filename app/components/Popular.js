f65cefd166839e671a3679b0a72b9cbcfba9eb9a
Partha Sarathi Nanda <partha.sarathi@consulting.active.ai>
2018-03-21 08:35:24 +0530

    Add pouplar grid component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props){
 		var languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python', 'Java'];
@@ -18,11 +19,38 @@ function SelectLanguage(props){
 		);
 }
 
+function RepoGrid(props){
+	return(
+		<ul className='popular-list'>
+			{props.repos.map(function(repo, index){
+				return (
+					<li key={repo.name} className='popular-item'>
+						<div className='popular-rank'>#{index + 1}</div>
+						<ul className="space-list-items">
+							<li>
+								<img className='avatar' src={repo.owner.avatar_url} alt='avatar for repo owner'/>
+							</li>
+							<li>
+								<a href={repo.html_url}>{repo.name}</a>
+							</li>
+							<li>
+								@{repo.owner.login}
+							</li>
+							<li>{repo.stargazers_count} stars</li>
+						</ul>
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
 class Popular extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 		this.updateLanguage = this.updateLanguage.bind(this);
 	}
@@ -33,6 +61,18 @@ class Popular extends React.Component{
 				selectedLanguage: lang
 			}
 		})
+		api.fetchPopularRepos(lang)
+			.then(function(repos){
+				this.setState(function(){
+					return {
+						repos: repos
+					}
+				})
+			}.bind(this))
+	}
+
+	componentDidMount(){
+		this.updateLanguage(this.state.selectedLanguage);
 	}
 
 	render(){
@@ -42,6 +82,7 @@ class Popular extends React.Component{
 					selectedLanguage={this.state.selectedLanguage}
 					onSelect={this.updateLanguage}
 				/>
+				{!this.state.repos? <p>Loading</p>: <RepoGrid repos={this.state.repos}/>}
 			</div>	
 		)
 	}
@@ -50,6 +91,10 @@ class Popular extends React.Component{
 SelectLanguage.propTypes = {
 	selectedLanguage: PropTypes.string.isRequired,
 	onSelect: PropTypes.func.isRequired
+}
+
+RepoGrid.propTypes = {
+	repos: PropTypes.array.isRequired
 }
 
 module.exports = Popular;
